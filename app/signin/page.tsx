294dfff982fad94438fde452006d7a93b453fd46
Saurav Adhikari <sauravads123@gmail.com>
2023-08-17 21:27:42 +0545

    :lipstick: style : all the pieces fall together

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,6 +1,6 @@
 export default function SignIn() {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-start">
+      <main className="flex min-h-screen flex-col items-center justify-center">
         And so the journey begins...
       </main>
     )
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,6 @@
 export default function SignUp() {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-start">
+      <main className="flex min-h-screen flex-col items-center justify-center">
         So...you want to embark on a new journey?
       </main>
     )
