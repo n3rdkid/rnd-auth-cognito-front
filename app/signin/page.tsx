30355e22f5e60be2475e895a1a1c646c6aa11b34
Saurav Adhikari <sauravads123@gmail.com>
2023-08-17 21:25:37 +0545

    :sparkles: feat : the journey of a million miles starts...

---
+++ app/signin/page.tsx
@@ -0,0 +1,8 @@
+export default function SignIn() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-start">
+        And so the journey begins...
+      </main>
+    )
+  }
+  
