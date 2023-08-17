fec75fd434157feb80fae7b2e5fdfa6ba0a3edb1
Saurav Adhikari <sauravads123@gmail.com>
2023-08-17 21:24:21 +0545

    :sparkles: feat : so you want to embark on this new journey

---
+++ app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SignUp() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-start">
+        So...you want to embark on a new journey?
+      </main>
+    )
+  }
+  
